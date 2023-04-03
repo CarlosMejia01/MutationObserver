1423d0d1bb7d05317a5864c3e60c4cdb9cfa49cb
Carlos Mejia <mrcarlitos007@gmail.com>
2023-04-02 19:22:47 -0500

    feat: Configure observer and listen when new element is added to the dom

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-alert('app.js');
+const observer = new MutationObserver((mutationList) => {
+    mutationList.forEach((mutation) => {
+        //Add
+        if(mutation.addedNodes.length) {
+            console.log('Add new element', mutation.addedNodes[0]);
+        }
+    });
+});
+
+// Target for listening
+const teams = document.querySelector('ul.teams');
+const observerOptions = {
+    attributes: true, //Change some atribbute
+    childList: true, //Listen when elements are added or removed from dom
+    subtree: false //Listen children of parent element
+};
+
+observer.observe(teams, observerOptions);
